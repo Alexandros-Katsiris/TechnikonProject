7b27180754439941326b6d8c6ea41e277a349dbc
Alexandros Katsiris <alexandros.katsiris@scytalys.com>
2023-06-14 09:46:02 +0300

    First Commit

---
+++ src/main/java/com/example/technikonproject/TechnikonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.technikonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnikonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnikonProjectApplication.class, args);
+    }
+
+}
