18695aebe5061ed97e6bd371348f104621be1a93
konstantinosk <konstantinos.kosfitskas@scytalys.com>
2023-07-04 10:58:34 +0300

    validation email/dependency

--- src/main/java/com/example/technikonproject/domain/WebUser.java
+++ src/main/java/com/example/technikonproject/domain/WebUser.java
@@ -2,16 +2,17 @@ package com.example.technikonproject.domain;
 
 import com.example.technikonproject.domain.subDomain.Address;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@Table(name = "webusers")
 public class WebUser {
 
     @Id
@@ -27,6 +28,7 @@ public class WebUser {
     private Address address;
     @Column(nullable = false)
     private Integer phoneNumber;
+    @Email
     @Column(nullable = false)
     private String email;
     @Column(nullable = false)
